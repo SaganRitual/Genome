26e62b6b08f3d3db5697107dcd3bda4b7661fcc4
Rob Bishop <c.sagan.ritual@gmail.com>
2021-03-13 20:48:01 -0800

    Proper tests for random-buffer generator

--- Genome/main.swift
+++ Genome/main.swift
@@ -4,23 +4,43 @@ import Foundation
 
 print("Hello, World!")
 
-func testGenomeAndMutator() {
-    let p1 = Genome(cGenes: 10)
-    print(p1.genes.map { $0 })
-
-    let p2 = Genome(exactCopyFrom: p1)
-    print(p2.genes.map { $0 })
-
-    let p3 = Genome(cloneFrom: p1)
-    print(p3.genes.map { $0 })
-
-    let p4 = Genome(mate: p1, with: p2)
-    print(p4.genes.map { $0 })
-
-    for _ in 0..<100 {
-        let p = Genome(mate: p1, with: p2)
-        print(p.genes.map { $0 })
-    }
+enum World {
+    static let cSamples = 10_000
+    static let staticRandomer = StaticRandomer(cSamples)
 }
 
-testGenomeAndMutator()
+func testBellCurveishness() {
+    // Output from this loop should look bell-curve-ish.
+    // Don't forget the generator is really noisy
+    var iter = StaticRandomerIterator(randomer: World.staticRandomer, .gaussian)
+    let cBuckets = 10
+    var buckets = [Double](repeating: 0, count: cBuckets)
+
+    for _ in 0..<10_000 {
+        let raw = iter.next()!
+        let sample = Double(raw + 1) / 2  // -1..<1 scaled to 0..<1
+        let bucket = Int(sample * Double(cBuckets))
+        buckets[bucket] += 1
+    }
+
+    print(buckets)
+}
+
+func testRandomLookingness() {
+    // Output from this loop should look like random (similar counts in each bucket)
+    var iter = StaticRandomerIterator(randomer: World.staticRandomer, .random)
+    let cBuckets = 10
+    var buckets = [Double](repeating: 0, count: cBuckets)
+
+    for _ in 0..<10_000 {
+        let raw = iter.next()!
+        let sample = Double(raw + 1) / 2  // -1..<1 scaled to 0..<1
+        let bucket = Int(sample * Double(cBuckets))
+        buckets[bucket] += 1
+    }
+
+    print(buckets)
+}
+
+testBellCurveishness()
+testRandomLookingness()
--- StaticRandomer.swift
+++ StaticRandomer.swift
@@ -21,9 +21,17 @@ class StaticRandomer {
         randomSamples.deallocate()
     }
 
+    func gaussrand(_ cIterations: Int) -> Double {
+        let c = Double(cIterations)
+
+        return ((0..<cIterations).reduce(Double.zero) { subtotal, _ in
+            subtotal + Double.random(in: -1...1)
+        } - c / 2) / sqrt(c / 12)
+    }
+
     init(_ cSamplesToGenerate: Int) {
-        let gs = UnsafeMutableBufferPointer<Element>.allocate(capacity: cSamplesToGenerate)
-        let rs = UnsafeMutableBufferPointer<Element>.allocate(capacity: cSamplesToGenerate)
+        var gs = UnsafeMutableBufferPointer<Element>.allocate(capacity: cSamplesToGenerate)
+        var rs = UnsafeMutableBufferPointer<Element>.allocate(capacity: cSamplesToGenerate)
 
         gaussianSamples = UnsafeBufferPointer(gs)
         randomSamples = UnsafeBufferPointer(rs)
@@ -41,14 +49,19 @@ class StaticRandomer {
         )
 
         (0..<cSamplesToGenerate).forEach { sampleSS in
-            gs[sampleSS] =
-                Element(gaussianDistribution.nextInt()) /
-                Element(distributionRange.upperBound)
-
-            rs[sampleSS] =
-                Element(randomDistribution.nextInt()) /
-                Element(distributionRange.upperBound)
+            gs[sampleSS] = Element(gaussianDistribution.nextInt())
+            rs[sampleSS] = Element(randomDistribution.nextInt())
         }
+
+        // Various hoop-jumping I've done to get smooth gaussian samples.
+        // Nothing works, they're noisy as hell, the best thing to do is
+        // generate a lot of them ahead of time and normalize them
+        let mustBeLessThan1: Float = 1 + 1e-5
+        let gmax = vDSP.maximumMagnitude(gs) * mustBeLessThan1
+        let rmax = vDSP.maximumMagnitude(rs) * mustBeLessThan1
+
+        vDSP.divide(gs, gmax, result: &gs)
+        vDSP.divide(rs, rmax, result: &rs)
     }
 
     func getBuffer(_ mode: Mode, cElements: Int) -> UnsafeBufferPointer<Element> {
